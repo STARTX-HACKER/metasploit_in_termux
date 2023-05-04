475586d1f95bdb05f7babf85d5cf58e23f5f1621
STARTX HACKER <130206716+STARTX-HACKER@users.noreply.github.com>
2023-05-05 02:39:34 +0300

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,6 +1,6 @@
 pkg install wget
 wget -P $HOME/ https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
-chmod +x $HEME/metasploit.sh
+chmod +x $HOME/metasploit.sh
 bash $HOME/metasploit.sh
 rm $HOME/metasploit-framework/Gemfile.lock
 cd $HOME/metasploit-framework/
