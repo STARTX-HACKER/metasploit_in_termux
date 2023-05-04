f8c07451e1878d1149938b952ea3c16f876b022e
STARTX HACKER <130206716+STARTX-HACKER@users.noreply.github.com>
2023-05-04 15:05:56 +0300

    Update msf_install.sh

--- msf_install.sh
+++ msf_install.sh
@@ -2,3 +2,5 @@ pkg install wget
 wget -P $HOME/ https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
 chmod +x $HEME/metasploit_in_termux/metasploit.sh
 bash $HOME/metasploit_in_termux/metasploit.sh
+rm $HOME/metasploit-framework/Gemfile.lock
+cd $HOME/metasploit-framework/
